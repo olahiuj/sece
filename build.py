46473af7e28f519c05413bbd01239754412bc9b1
jpwang <jpwang@smail.nju.edu.cn>
2022-11-15 02:04:56 +0800

    upd: build script to support cli-app usage.

--- build.py
+++ build.py
@@ -7,11 +7,10 @@ use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
 
-
 name = "sece"
 default_task = "publish"
 
 
 @init
 def set_properties(project):
-    pass
+    project.set_property("distutils_console_scripts", ["sece = main:main"])
